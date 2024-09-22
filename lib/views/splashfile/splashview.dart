fbab78c3bc42bbd5d08bf33d8a8cfaa4be42d377
Ayamohamedkhalil <ayak78118@gmail.com>
2024-09-23 01:27:25 +0300

    las

--- lib/views/splashfile/splashview.dart
+++ lib/views/splashfile/splashview.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
+
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:splash_onboarding_test/views/splashfile/splashbody.dart';
 
