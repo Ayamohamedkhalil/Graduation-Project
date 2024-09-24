86bacc57a689b5ea315a2547e168751c0f88bdaf
Ayamohamedkhalil <ayak78118@gmail.com>
2024-09-25 02:13:04 +0300

    depandgeneralareresponsive

--- lib/views/InstructionScreen.dart
+++ lib/views/InstructionScreen.dart
@@ -5,237 +5,225 @@ import 'package:splash_onboarding_test/Registeration/registeration.dart';
 class IntroScreen extends StatelessWidget {
   IntroScreen({super.key});
 
-  final List<PageViewModel> pages = [
-    PageViewModel(
-      titleWidget: Padding(
-        padding: const EdgeInsets.only(top: 160.0),
-        child: Text(
-          'Welcome to MALAZ',
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.w500,
-            color: Colors.white.withOpacity(.85),
-            fontFamily: 'Ledger',
-          ),
-          textAlign: TextAlign.center,
-        ),
-      ),
+  final List<PageViewModel> pages = [];
 
-      bodyWidget: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20),
-            child: Image.asset(
-              'assets/onBoarding.png',
-              width: 300,
-            ),
-          ),
-          const SizedBox(
-            height: 70,
-          ),
-          const Text(
-            'your personal mental\nwellness companion',
+  @override
+  Widget build(BuildContext context) {
+    // Get screen height and width for responsive sizing
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    // Initialize pages with responsive sizing
+    final List<PageViewModel> pages = [
+      PageViewModel(
+        titleWidget: Padding(
+          padding: EdgeInsets.only(top: screenHeight * 0.2),
+          child: Text(
+            'Welcome to MALAZ',
             style: TextStyle(
-              color: Colors.white,
-              fontSize: 18,
+              fontSize: screenWidth * 0.08,
+              fontWeight: FontWeight.w500,
+              color: Colors.white.withOpacity(.85),
               fontFamily: 'Ledger',
             ),
             textAlign: TextAlign.center,
           ),
-        ],
-      ),
-      //image: Image.asset('assets/Intro1.png'),
-      decoration: const PageDecoration(
-        pageColor: Color(0xFF537F5C),
-        titlePadding: EdgeInsets.all(0),
-        contentMargin: EdgeInsets.all(16),
-        bodyPadding: EdgeInsets.only(top: 0),
-      ),
-    ),
-     PageViewModel(
-      title: '',
-      bodyWidget: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-         // SizedBox(height: 10),
-          // Background image with decoration
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              // Decorative background image
-              Image.asset(
-                'assets/updateintro.png', // Make sure this asset path is correct
-                fit: BoxFit.contain,
+        ),
+        bodyWidget: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: screenHeight * 0.05,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: screenWidth * 0.05),
+              child: Image.asset(
+                'assets/onBoarding.png',
+                width: screenWidth * 0.8,
               ),
-              // Centered text
-             const Text(
-              'Our app is designed\nto help you \nunderstand and\n manage your mental health\nthrough a variety of \ndiagnostic tests',
+            ),
+            SizedBox(
+              height: screenHeight * 0.1,
+            ),
+            Text(
+              'your personal mental\nwellness companion',
               style: TextStyle(
-                fontFamily: 'Ledger',
-                fontSize: 23,
                 color: Colors.white,
+                fontSize: screenWidth * 0.05,
+                fontFamily: 'Ledger',
               ),
               textAlign: TextAlign.center,
             ),
-            ],
-          ),
-        ],
+          ],
+        ),
+        decoration: const PageDecoration(
+          pageColor: Color(0xFF537F5C),
+          titlePadding: EdgeInsets.all(0),
+          contentMargin: EdgeInsets.all(16),
+          bodyPadding: EdgeInsets.only(top: 0),
+        ),
       ),
-      decoration: const PageDecoration(
-        pageColor: Color(0xFF537F5C), // Matching the background color
-      ),
-    ),
-    
-    PageViewModel(
-      title: '', // Empty since the design does not include a title
-      bodyWidget: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
+      PageViewModel(
+        title: '',
+        bodyWidget: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            // First section - Take a test
-            Row(children: [
-              Image.asset('assets/introonboarding1.png',
-                  height: 120), // Example image
-              const SizedBox(
-                width: 70,
-              ),
-              Text(
-                'Take\n   a\n test',
-                style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white.withOpacity(.85),
-                    fontFamily: 'Ledger'),
-              ),
-            ]),
-            //SizedBox(height: 110), // Space between sections
-            Image.asset(
-              'assets/Line27.png',
-              height: 110,
-            ),
-            // Second section - Look at the Guidelines
-            Row(
+            Stack(
+              alignment: Alignment.center,
               children: [
-                // Example image
-
-                Text(
-                  '   Look at \n       the\n Guidelines',
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.white.withOpacity(.85),
-                      fontFamily: 'Ledger'),
-                ),
-                const SizedBox(
-                  width: 50,
-                ),
-                Image.asset('assets/introonboarding2.png', height: 120),
-              ],
-            ),
-            Image.asset(
-              'assets/Line 28.png',
-              height: 110,
-            ),
-            //SizedBox(height: 110), // Space between sections
-
-            // Third section - Write what you think
-            Row(
-              children: [
-                Image.asset('assets/introonboarding3.png',
-                    height: 120), // Example image
-                const SizedBox(
-                  width: 50,
+                Image.asset(
+                  'assets/updateintro.png',
+                  fit: BoxFit.contain,
                 ),
                 Text(
-                  'Write what\n you think',
+                  'Our app is designed\nto help you \nunderstand and\n manage your mental health\nthrough a variety of \ndiagnostic tests',
                   style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.white.withOpacity(.85),
-                      fontFamily: 'Ledger'),
+                    fontFamily: 'Ledger',
+                    fontSize: screenWidth * 0.06,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
               ],
             ),
           ],
         ),
+        decoration: const PageDecoration(
+          pageColor: Color(0xFF537F5C),
+        ),
       ),
-      decoration: const PageDecoration(
-        pageColor:
-            Color(0xFF537F5C), // Background color similar to the screenshot
-      ),
-    ),
-    PageViewModel(
-      title: '',
-      bodyWidget: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SizedBox(height: 50),
-          // Background image with decoration
-          Stack(
-            alignment: Alignment.center,
+      PageViewModel(
+        title: '',
+        bodyWidget: Padding(
+          padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.04),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              // Decorative background image
-              Image.asset(
-                'assets/intro4.png', // Make sure this asset path is correct
-                fit: BoxFit.contain,
-              ),
-              // Centered text
-              const Text(
-                ' Your\n Journey\n Starts\n  Now',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 30,
-                  fontWeight: FontWeight.w500,
-                  // height: 1.4,
-                  fontFamily: 'Ledger', // Customize as per your font choice
+              Row(children: [
+                Image.asset(
+                  'assets/introonboarding1.png',
+                  height: screenHeight * 0.15,
                 ),
-                textAlign: TextAlign.center,
+                SizedBox(
+                  width: screenWidth * 0.2,
+                ),
+                Text(
+                  'Take\n   a\n test',
+                  style: TextStyle(
+                    fontSize: screenWidth * 0.06,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white.withOpacity(.85),
+                    fontFamily: 'Ledger',
+                  ),
+                ),
+              ]),
+              SizedBox(height: screenHeight * 0.05),
+              Image.asset(
+                'assets/Line27.png',
+                height: screenHeight * 0.13,
+              ),
+              Row(
+                children: [
+                  Text(
+                    '   Look at \n       the\n Guidelines',
+                    style: TextStyle(
+                      fontSize: screenWidth * 0.05,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white.withOpacity(.85),
+                      fontFamily: 'Ledger',
+                    ),
+                  ),
+                  SizedBox(
+                    width: screenWidth * 0.15,
+                  ),
+                  Image.asset('assets/introonboarding2.png',
+                      height: screenHeight * 0.15),
+                ],
+              ),
+              Image.asset(
+                'assets/Line 28.png',
+                height: screenHeight * 0.13,
+              ),
+              Row(
+                children: [
+                  Image.asset('assets/introonboarding3.png',
+                      height: screenHeight * 0.15),
+                  SizedBox(
+                    width: screenWidth * 0.15,
+                  ),
+                  Text(
+                    'Write what\n you think',
+                    style: TextStyle(
+                      fontSize: screenWidth * 0.05,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white.withOpacity(.85),
+                      fontFamily: 'Ledger',
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
+        ),
+        decoration: const PageDecoration(
+          pageColor: Color(0xFF537F5C),
+        ),
       ),
-      decoration: const PageDecoration(
-        pageColor: Color(0xFF537F5C), // Matching the background color
+      PageViewModel(
+        title: '',
+        bodyWidget: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: screenHeight * 0.05),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Image.asset(
+                  'assets/intro4.png',
+                  fit: BoxFit.contain,
+                ),
+                Text(
+                  ' Your\n Journey\n Starts\n  Now',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: screenWidth * 0.08,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: 'Ledger',
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ],
+        ),
+        decoration: const PageDecoration(
+          pageColor: Color(0xFF537F5C),
+        ),
       ),
-    ),
-  ];
+    ];
 
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Color(0xFF537F5C), // Set the background color of the Scaffold
       body: IntroductionScreen(
         globalBackgroundColor: const Color(0xFF537F5C),
         pages: pages,
-        dotsDecorator: const DotsDecorator(
-          size: Size(10, 10),
+        dotsDecorator: DotsDecorator(
+          size: Size(screenWidth * 0.03, screenWidth * 0.03),
           color: Colors.white,
-          activeSize: Size.square(15),
+          activeSize: Size.square(screenWidth * 0.04),
           activeColor: Colors.grey,
         ),
         showDoneButton: true,
-        done: const Text(
+        done: Text(
           'Done',
-          style: TextStyle(fontSize: 20, color: Colors.white),
+          style: TextStyle(fontSize: screenWidth * 0.05, color: Colors.white),
         ),
         showSkipButton: true,
-        skip: const Text(
+        skip: Text(
           'Skip',
-          style: TextStyle(fontSize: 20, color: Colors.white),
+          style: TextStyle(fontSize: screenWidth * 0.05, color: Colors.white),
         ),
         showNextButton: false,
-        /*next: const Icon(
-          Icons.arrow_forward,
-          size: 20,
-          color: Colors.black,
-        ),*/
         onDone: () => onDone(context),
         onSkip: () => onSkip(context),
       ),
@@ -246,9 +234,9 @@ class IntroScreen extends StatelessWidget {
     Navigator.pushReplacement(context,
         MaterialPageRoute(builder: (context) => const Registeration()));
   }
-}
 
-void onSkip(context) {
-  Navigator.pushReplacement(
-      context, MaterialPageRoute(builder: (context) => const Registeration()));
+  void onSkip(context) {
+    Navigator.pushReplacement(context,
+        MaterialPageRoute(builder: (context) => const Registeration()));
+  }
 }
