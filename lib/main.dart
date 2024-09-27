aff3d0116e28d7548b2ece14a091d34469ee4da5
Ayamohamedkhalil <ayak78118@gmail.com>
2024-09-27 19:10:27 +0300

    updatefirebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:splash_onboarding_test/firebase_options.dart';
+import 'package:splash_onboarding_test/views/firebase_notifications/firebase_notifications.dart';
 
 //import 'package:splash_onboarding_test/home.dart';
 //import 'package:device_preview/device_preview.dart';
@@ -8,9 +9,8 @@ import 'package:splash_onboarding_test/views/splashfile/splashview.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options:DefaultFirebaseOptions.currentPlatform
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await firebasenotifications().initnotifications();
   runApp(test_app());
 }
 
