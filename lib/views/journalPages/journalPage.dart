247b0d1b85b628bc66a7b412610bbcf8884a1ed7
Ayamohamedkhalil <ayak78118@gmail.com>
2024-10-04 23:11:08 +0300

    trial

--- lib/views/journalPages/journalPage.dart
+++ lib/views/journalPages/journalPage.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:splash_onboarding_test/components/ButtonBar.dart';
+import 'package:splash_onboarding_test/views/ConatctUspage/contactUsPage.dart';
 import 'package:splash_onboarding_test/views/journalPages/journal1.dart';
-
-import 'package:splash_onboarding_test/views/journalPages/journalYearPage.dart';
 import 'package:splash_onboarding_test/views/journalPages/widgets/JournalData.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/intl.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class JournalPage extends StatefulWidget {
   const JournalPage({super.key});
@@ -21,6 +22,57 @@ class _JournalPageState extends State<JournalPage> {
   DateTime focusedDay = DateTime.now();
   DateTime selectedYear = DateTime.now();
 
+  List<dynamic> journals = []; // List to store fetched journals
+  String errorMessage = ''; // Variable to store error messages
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchJournals(); // Fetch all journals when the page loads
+  }
+
+  Future<void> _fetchJournals(
+      {String? year, String? month, String? day}) async {
+    final String? token = await getToken();
+    
+    setState(() {
+      errorMessage = ''; // Reset error message on every fetch attempt
+    });
+    try {
+      final response = await http.post(
+        Uri.parse(
+            'https://backend-production-19d7.up.railway.app/api/get-journals'),
+        headers: {
+          'Authorization': token ?? '',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({
+          'year': year,
+          'month': month,
+          'day': day,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        setState(() {
+          journals = data['journals'] ?? [];
+          if (journals.isEmpty) {
+            errorMessage = 'No journals found for the selected date.';
+          }
+        });
+      } else {
+        setState(() {
+          errorMessage = 'Failed to fetch journals.';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        errorMessage = 'Error: $e';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,16 +81,11 @@ class _JournalPageState extends State<JournalPage> {
         color: const Color(0xff537F5C),
         child: Stack(children: [
           SingleChildScrollView(
-            padding: const EdgeInsets.only(
-                bottom: 80), // Extra padding at the bottom
+            padding: const EdgeInsets.only(bottom: 80),
             child: Column(
               children: [
-                const SizedBox(
-                  height: 50,
-                ),
-                //
-                //SearchBar
-                //
+                const SizedBox(height: 50),
+                // SearchBar
                 SizedBox(
                   height: 40,
                   width: MediaQuery.of(context).size.width * 0.9,
@@ -52,9 +99,10 @@ class _JournalPageState extends State<JournalPage> {
                           const Icon(Icons.mic_none, color: Colors.white),
                       hintText: 'Search for test',
                       hintStyle: const TextStyle(
-                          color: Color(0xffD9D9D9),
-                          fontFamily: 'Ledger',
-                          fontSize: 18),
+                        color: Color(0xffD9D9D9),
+                        fontFamily: 'Ledger',
+                        fontSize: 18,
+                      ),
                       contentPadding: const EdgeInsets.symmetric(vertical: 2.0),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(25.0),
@@ -80,9 +128,8 @@ class _JournalPageState extends State<JournalPage> {
                     ),
                   ),
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
+                const SizedBox(height: 20),
+                // Welcome Text
                 Column(
                   children: [
                     Row(
@@ -91,16 +138,17 @@ class _JournalPageState extends State<JournalPage> {
                         Text(
                           "Welcome John!",
                           style: TextStyle(
-                              color: Color(0xffAFAC7F),
-                              fontSize: 29,
-                              fontFamily: 'Ledger'),
+                            color: Color(0xffAFAC7F),
+                            fontSize: 29,
+                            fontFamily: 'Ledger',
+                          ),
                         ),
                         GestureDetector(
                           onTap: () {
                             Navigator.push(
                               context,
                               MaterialPageRoute(
-                                builder: (context) =>NewJournalEntryPage(
+                                builder: (context) => NewJournalEntryPage(
                                     currentDate: selectedDay),
                               ),
                             );
@@ -113,48 +161,26 @@ class _JournalPageState extends State<JournalPage> {
                         ),
                       ],
                     ),
-                    SizedBox(
-                      height: 3,
-                    ),
+                    SizedBox(height: 3),
                     Padding(
                       padding: EdgeInsets.only(left: 7),
                       child: Text(
                         "Malaz thinks you are about to make something great",
                         softWrap: true,
                         style: TextStyle(
-                            color: Color.fromARGB(216, 255, 255, 255),
-                            fontSize: 18),
+                          color: Color.fromARGB(216, 255, 255, 255),
+                          fontSize: 18,
+                        ),
                       ),
                     )
                   ],
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                //
-                //calender
-                //
+                const SizedBox(height: 10),
+                // Calendar
                 TableCalendar(
                   daysOfWeekStyle: DaysOfWeekStyle(
                     dowTextFormatter: (date, locale) {
-                      switch (DateFormat('E', locale).format(date)) {
-                        case "Sun":
-                          return "S";
-                        case "Mon":
-                          return "M";
-                        case "Tue":
-                          return "T";
-                        case "Wed":
-                          return "W";
-                        case "Thu":
-                          return "T";
-                        case "Fri":
-                          return "F";
-                        case "Sat":
-                          return "S";
-                        default:
-                          return DateFormat('E', locale).format(date);
-                      }
+                      return DateFormat('E', locale).format(date)[0];
                     },
                     weekdayStyle: const TextStyle(
                         color: Colors.white, fontFamily: 'Ledger'),
@@ -162,133 +188,115 @@ class _JournalPageState extends State<JournalPage> {
                         color: Colors.white, fontFamily: 'Ledger'),
                   ),
                   calendarStyle: CalendarStyle(
-                      cellMargin: const EdgeInsets.symmetric(
-                          horizontal: 13, vertical: 13),
-                      todayTextStyle: const TextStyle(color: Colors.white),
-                      todayDecoration: BoxDecoration(
-                          color: const Color.fromARGB(68, 217, 217, 217),
-                          border: Border.all(color: Colors.white),
-                          borderRadius: BorderRadius.circular(100)),
-                      selectedTextStyle: const TextStyle(
-                        color: Colors.black,
-                      ),
-                      selectedDecoration: const BoxDecoration(
-                        color: Colors.white,
-                        shape: BoxShape.circle,
-                      ),
-                      defaultTextStyle: const TextStyle(color: Colors.white),
-                      outsideTextStyle: const TextStyle(
-                          color: Color.fromARGB(85, 255, 255, 255)),
-                      weekendTextStyle: const TextStyle(color: Colors.white)),
+                    todayDecoration: BoxDecoration(
+                      color: const Color.fromARGB(68, 217, 217, 217),
+                      border: Border.all(color: Colors.white),
+                      shape: BoxShape.circle, // Fixed to use circle shape
+                    ),
+                    selectedDecoration: const BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle, // Circle shape does not need borderRadius
+                    ),
+                    defaultTextStyle: const TextStyle(color: Colors.white),
+                  ),
                   headerStyle: HeaderStyle(
-                    rightChevronIcon: const Icon(
-                      Icons.chevron_right,
-                      color: Colors.white,
-                    ),
-                    leftChevronIcon: const Icon(
-                      Icons.chevron_left,
-                      color: Colors.white,
-                    ),
                     titleTextFormatter: (date, locale) =>
                         DateFormat('MMM : yyyy', locale).format(date),
-                    leftChevronMargin: EdgeInsets.only(
-                        left: MediaQuery.of(context).size.width * 0.1),
-                    rightChevronMargin: EdgeInsets.only(
-                        right: MediaQuery.of(context).size.width * 0.1),
-                    formatButtonVisible: false,
-                    titleCentered: true,
+                    leftChevronIcon:
+                        const Icon(Icons.chevron_left, color: Colors.white),
+                    rightChevronIcon:
+                        const Icon(Icons.chevron_right, color: Colors.white),
                     titleTextStyle: const TextStyle(
                       fontSize: 23,
                       color: Colors.white,
                       fontFamily: 'Ledger',
                     ),
-                    leftChevronVisible: true,
-                    rightChevronVisible: true,
+                    titleCentered: true,
+                    formatButtonVisible: false,
                   ),
-                  availableCalendarFormats: const {
-                    CalendarFormat.month: 'Month',
-                  },
                   firstDay: DateTime.utc(2020, 1, 1),
                   lastDay: DateTime.utc(2050, 12, 31),
                   focusedDay: focusedDay,
                   calendarFormat: calendarFormat,
-                  selectedDayPredicate: (day) {
-                    return day == selectedDay ? true : false;
-                  },
+                  selectedDayPredicate: (day) => isSameDay(day, selectedDay),
                   onDaySelected: (selectedDay, focusedDay) {
                     setState(() {
                       this.selectedDay = selectedDay;
                       this.focusedDay = focusedDay;
                     });
-                   /* Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            JournalPageDay(selectDay: selectedDay),
-                      ),
-                    );*/
+                    _fetchJournals(
+                      year: selectedDay.year.toString(),
+                      month: selectedDay.month.toString().padLeft(2, '0'),
+                      day: selectedDay.day.toString().padLeft(2, '0'),
+                    );
+                  },
+                  onHeaderTapped: (date) {
+                    setState(() {
+                      selectedYear = date;
+                    });
+                    _fetchJournals(
+                      year: selectedYear.year.toString(),
+                      month: selectedYear.month.toString().padLeft(2, '0'),
+                    );
                   },
                   onPageChanged: (focusedDay) {
                     this.focusedDay = focusedDay;
                   },
-                  onHeaderTapped: (Date) {
-                    setState(() {
-                      selectedYear = Date;
-                    });
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Journalyearpage(
-                          selectYear: selectedYear.year,
-                        ),
-                      ),
-                    );
-                  },
                 ),
-                //
-                // Month Journals
-                //
+                // Displaying Fetched Journals
                 const Padding(
                   padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-                  child: Column(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Month Journals",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 30,
-                                fontFamily: 'Ledger'),
-                          ),
-                        ],
+                      Text(
+                        "Month Journals",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontFamily: 'Ledger',
+                        ),
                       ),
                     ],
                   ),
                 ),
                 SizedBox(
                   height: 250,
-                  child: ListView.builder(
-                    padding: const EdgeInsets.only(
-                        bottom: 80), // Add bottom padding here
-                    itemBuilder: (context, index) {
-                      return const JournalData(
-                          title: "I was wondering", date: "14/1/2024");
-                    },
-                  ),
+                  child: journals.isEmpty
+                      ? Center(
+                          child: Text(
+                            errorMessage.isNotEmpty
+                                ? errorMessage
+                                : 'No journals available.',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                            ),
+                          ),
+                        )
+                      : ListView.builder(
+                          padding: const EdgeInsets.only(bottom: 80),
+                          itemCount: journals.length,
+                          itemBuilder: (context, index) {
+                            final journal = journals[index];
+                            return JournalData(
+                              title:
+                                  journal['entries'][0]['title'] ?? 'No Title',
+                              date: journal['date'] ?? 'No Date',
+                            );
+                          },
+                        ),
                 ),
-                const SizedBox(
-                  height: 5,
-                ),
+                const SizedBox(height: 5),
               ],
             ),
           ),
           Positioned(
-              bottom: 5,
-              left: MediaQuery.of(context).size.width * 0.15,
-              right: MediaQuery.of(context).size.width * 0.15,
-              child: const BarButton())
+            bottom: 5,
+            left: MediaQuery.of(context).size.width * 0.15,
+            right: MediaQuery.of(context).size.width * 0.15,
+            child: const BarButton(),
+          ),
         ]),
       ),
     );
